e7b126bfa9b0cb093f8b448ef1fc918f0f1ca19f
shibumi2906 <539620915@mail.ru>
2024-03-29 19:44:24 +0300

    new
    изменила размер мишени, внесли картинку мишени

--- mayn.py
+++ mayn.py
@@ -13,10 +13,10 @@ pygame.display.set_caption("игра ТИР")
 icon = pygame.image.load("img/2906.png")
 pygame.display.set_icon(icon)
 #  создаём мишень
-target_image = pygame.image.load(“img/”)
+target_image = pygame.image.load("img/1973")
 #даём размер мишени
-target_width = 50
-target_height = 50
+target_width = 80
+target_height = 80
 target_x = ()
 # задаём рандомный цвет фона
 color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
